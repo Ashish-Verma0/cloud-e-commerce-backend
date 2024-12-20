c627bad6d237d5350eda2e40f8bf8df6979ec449
Ashish-Verma0 <av200280@gmail.com>
2024-12-21 00:42:34 +0530

    adding ts-node in package.json file

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "../public/userImage")));
+app.use("/", express.static(path.join(__dirname, "../public/shopLogo")));
 app.use("/user", userRoute);
 app.use("/seller", sellerRoute);
 
